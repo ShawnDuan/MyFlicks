c379ee937c37881ba5ecdbc093b3acb1f50893be
Shawn Duan <duan1216@gmail.com>
2016-10-15 15:30:51 -0700

    Pull to refresh.

--- app/src/main/java/com/example/sduan/myflicks/MainActivity.java
+++ app/src/main/java/com/example/sduan/myflicks/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.sduan.myflicks;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.sduan.myflicks.adapters.MovieAdapter;
 import com.example.sduan.myflicks.models.Movie;
@@ -31,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Movie> mMovies;
     MovieAdapter mMovieAdapter;
     RecyclerView mRecyclerView;
+    SwipeRefreshLayout mSwipeContainer;
+    final AsyncHttpClient mClient = new AsyncHttpClient();
+    final String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +41,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestMovieListAsync();
+                Toast.makeText(MainActivity.this, "Refreshing movie list completed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         mMovies = new ArrayList<>();
         mRecyclerView = (RecyclerView) findViewById(R.id.rvMovies);
         mMovieAdapter = new MovieAdapter(MainActivity.this, mMovies);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mMovieAdapter);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                JSONArray movieJsonResults;
-                try {
-                    movieJsonResults = response.getJSONArray("results");
-                    mMovies.addAll(Movie.fromJSONArray(movieJsonResults));
-                    mMovieAdapter.notifyDataSetChanged();
-                    Log.d(TAG, "movieJsonResults: " + mMovies.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
-        });
+        requestMovieListAsync();
     }
 
     @Override
@@ -88,5 +79,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void requestMovieListAsync() {
+        mClient.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJsonResults;
+                try {
+                    movieJsonResults = response.getJSONArray("results");
+                    mMovies.clear();
+                    mMovies.addAll(Movie.fromJSONArray(movieJsonResults));
+                    mMovieAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    mSwipeContainer.setRefreshing(false);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
     }
 }
